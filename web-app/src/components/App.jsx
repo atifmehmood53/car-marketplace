77e3e998be7ba13d6fe1a545c542b23fa9d7da15
Atif Mehmood <amehmood@stellic.com>
2024-01-27 13:46:25 +0500

    added charkra ui and app container component

---
+++ web-app/src/components/App.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import { Box, Container } from '@chakra-ui/react'
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <>
+      <Box bg="gray.100" h="100vh" overflow="scroll">
+        <NavBar />
+        <Container maxW="8xl" py="24">
+          <Outlet />
+        </Container>
+      </Box>
+    </>
+  )
+}
+
+export default App
