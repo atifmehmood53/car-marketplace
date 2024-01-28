2506e055d87407a29b6ce1fd134bb8d62bf4eb25
Atif Mehmood <amehmood@stellic.com>
2024-01-28 13:24:19 +0500

    added tradein model

---
+++ web-app/src/components/CarTradinModal.jsx
@@ -0,0 +1,35 @@
+// CarTradingModal.js
+
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import TradinForm from "./TradinForm";
+
+const CarTradingModal = ({ isOpen, onOpen, onClose }) => {
+  return (
+    <>
+      <Button onClick={onOpen} w="100%" size="lg" colorScheme="gray">
+        Tradin Your Car
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Car Trading Form</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <TradinForm />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default CarTradingModal;
